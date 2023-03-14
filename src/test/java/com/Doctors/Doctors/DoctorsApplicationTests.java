662755bd5e27dc856a7b6e799913a4a4402ff3dc
lmledingo <117645658+lmledingo@users.noreply.github.com>
2023-03-14 10:54:06 +0100

    first repo

---
+++ src/test/java/com/Doctors/Doctors/DoctorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Doctors.Doctors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
